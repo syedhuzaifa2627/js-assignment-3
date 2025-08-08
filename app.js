d343b487b864fc89497ea2d76f2a45ad436edb83
syedhuzaifa2627 <huzaifasyed2627@gmail.com>
2025-08-09 00:08:51 +0500

    second commit

---
+++ app.js
@@ -0,0 +1,51 @@
+// Chapter 9
+
+// Question 1
+
+//var city = prompt("Enter your city name:")
+//if (city === "Karachi") {
+//    alert("Welcome to city of lights")
+//}
+
+// Question 2
+
+//var gender = prompt("Enter your gender:")
+//var message = "";
+
+//if (gender === "male") {
+//    message = "Good Morning Sir"
+//}
+//if (gender === "female") {
+//    message = "Good Morning Ma’am"
+//}
+
+//alert(message);
+
+// Question 3
+
+//let color = prompt("Enter the color of the traffic signal:");
+//let rules = ""
+
+//if (color === "red") {
+//    rules = "Must Stop"
+//}
+//if (color === "yellow") {
+//    rules = "Ready to move"
+//}
+//if (color === "green") {
+//    rules = "Move now"
+//}
+
+//document.writeln("<table border='1' cellpadding='5'>")
+//document.writeln("<tr><th>Signal Color</th><th>Message</th></tr>")
+//document.writeln("<tr><td>" + color + "</td><td>" + rules + "</td></tr>")
+//document.writeln("</table>")
+
+// Question 4
+
+//var fuel = prompt("Enter remaining fuel in your car:")
+
+//if (fuel < 0.25) {
+//    alert("Please refill the fuel in your car")
+//}
+
